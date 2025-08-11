61befae8a3c3ad6cbeef76460321c2e4cc7d5114
Justin-K-Ellis <jlke45@gmail.com>
2025-08-11 17:22:34 +0900

    feat: link about and resume pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 
+import About from "./pages/About";
 import Home from "./pages/Home";
+import Resume from "./pages/Resume";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/resume" element={<Resume />} />
       </Routes>
     </BrowserRouter>
   );
