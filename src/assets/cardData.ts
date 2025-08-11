5b055df77b1b89af8dcf2948cd8276569db8ab62
Justin-K-Ellis <jlke45@gmail.com>
2025-08-11 22:54:59 +0900

    feat: fill out basic project data

---
+++ src/assets/cardData.ts
@@ -0,0 +1,30 @@
+import type { ICard } from "../types";
+
+const cardData: ICard[] = [
+  {
+    title: "Japan Events",
+    imgSrc: "",
+    description:
+      "An app that indexes events from around Japan and let's users find them with a rich search interface, save interesting events, and anonymously share them.",
+    repoLink: "https://github.com/Event-Curator/event-curator",
+    deploymentLink: "eventsjp.com",
+  },
+  {
+    title: "Recipe Wrangler",
+    imgSrc: "",
+    description:
+      "Allows users to search for recipes, creates check lists for ingredients and cooking directions. Logged in users can also save recipes and create their own.",
+    repoLink: "https://github.com/Justin-K-Ellis/recipe_wrangler_client",
+    deploymentLink: "https://recipe-wrangler-client.vercel.app/home",
+  },
+  {
+    title: "Washoku Walker",
+    imgSrc: "",
+    description:
+      "Helps users find great restaurants in Japan. Users who want to find a specific food can get a selection of restaurants by dish and area, or get a list of highly-rated restaurants near them.",
+    repoLink: "https://github.com/Justin-K-Ellis/Washoku-Walker",
+    deploymentLink: "https://washoku-walker.onrender.com/",
+  },
+];
+
+export default cardData;
