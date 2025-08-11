c4f0ef42fa5d4f3bad5dcb4ebe6c6ebbb3227280
Justin-K-Ellis <jlke45@gmail.com>
2025-08-11 21:48:59 +0900

    feat: scafold Layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import { BrowserRouter, Route, Routes } from "react-router";
 
 import About from "./pages/About";
 import Home from "./pages/Home";
+import Layout from "./layouts/Layout";
 import Resume from "./pages/Resume";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" index element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/resume" element={<Resume />} />
+        <Route element={<Layout />}>
+          <Route path="/" index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/resume" element={<Resume />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
