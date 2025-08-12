84aed55cbf562378b764dbffe0e80a58fd26cb19
Justin-K-Ellis <jlke45@gmail.com>
2025-08-12 09:12:00 +0900

    fix: move portfolio text to text.json

--- src/assets/cardData.ts
+++ src/assets/cardData.ts
@@ -1,29 +1,54 @@
 import type { ICard } from "../types";
+import text from "../assets/text.json";
 
 const cardData: ICard[] = [
   {
-    title: "Japan Events",
-    imgSrc: "",
-    description:
-      "An app that indexes events from around Japan and let's users find them with a rich search interface, save interesting events, and anonymously share them.",
-    repoLink: "https://github.com/Event-Curator/event-curator",
-    deploymentLink: "eventsjp.com",
+    title: text.portfolio.japanEvents.title,
+    imgSrc: text.portfolio.japanEvents.imgSrc,
+    description: text.portfolio.japanEvents.description,
+    repoLink: text.portfolio.japanEvents.repoLink,
+    deploymentLink: text.portfolio.japanEvents.deploymentLink,
+    stack: text.portfolio.japanEvents.stack,
   },
   {
-    title: "Recipe Wrangler",
-    imgSrc: "",
-    description:
-      "Allows users to search for recipes, creates check lists for ingredients and cooking directions. Logged in users can also save recipes and create their own.",
-    repoLink: "https://github.com/Justin-K-Ellis/recipe_wrangler_client",
-    deploymentLink: "https://recipe-wrangler-client.vercel.app/home",
+    title: text.portfolio.recipeWrangler.title,
+    imgSrc: text.portfolio.recipeWrangler.imgSrc,
+    description: text.portfolio.recipeWrangler.description,
+    repoLink: text.portfolio.recipeWrangler.repoLink,
+    deploymentLink: text.portfolio.recipeWrangler.deploymentLink,
+    stack: text.portfolio.recipeWrangler.stack,
   },
   {
-    title: "Washoku Walker",
-    imgSrc: "",
-    description:
-      "Helps users find great restaurants in Japan. Users who want to find a specific food can get a selection of restaurants by dish and area, or get a list of highly-rated restaurants near them.",
-    repoLink: "https://github.com/Justin-K-Ellis/Washoku-Walker",
-    deploymentLink: "https://washoku-walker.onrender.com/",
+    title: text.portfolio.washokuWalker.title,
+    imgSrc: text.portfolio.washokuWalker.imgSrc,
+    description: text.portfolio.washokuWalker.description,
+    repoLink: text.portfolio.washokuWalker.repoLink,
+    deploymentLink: text.portfolio.washokuWalker.deploymentLink,
+    stack: text.portfolio.washokuWalker.stack,
+  },
+  {
+    title: text.portfolio.personalSite.title,
+    imgSrc: text.portfolio.personalSite.imgSrc,
+    description: text.portfolio.personalSite.description,
+    repoLink: text.portfolio.personalSite.repoLink,
+    deploymentLink: text.portfolio.personalSite.deploymentLink,
+    stack: text.portfolio.personalSite.stack,
+  },
+  {
+    title: text.portfolio.courseManagment.title,
+    imgSrc: text.portfolio.courseManagment.imgSrc,
+    description: text.portfolio.courseManagment.description,
+    repoLink: text.portfolio.courseManagment.repoLink,
+    deploymentLink: text.portfolio.courseManagment.deploymentLink,
+    stack: text.portfolio.courseManagment.stack,
+  },
+  {
+    title: text.portfolio.pokemonMemory.title,
+    imgSrc: text.portfolio.pokemonMemory.imgSrc,
+    description: text.portfolio.pokemonMemory.description,
+    repoLink: text.portfolio.pokemonMemory.repoLink,
+    deploymentLink: text.portfolio.pokemonMemory.deploymentLink,
+    stack: text.portfolio.pokemonMemory.stack,
   },
 ];
 
