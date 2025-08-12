1374321a08af584c23e678685b98a0b9e1055a8f
Justin-K-Ellis <jlke45@gmail.com>
2025-08-12 15:01:31 +0900

    feat: stylize navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,14 +2,26 @@ import { Link } from "react-router";
 
 export default function Navbar() {
   return (
-    <nav className="border-b border-accent shadow">
+    <nav className=" shadow flex justify-between items-center bg-primary p-2 md:p-4">
       <Link to={"/"}>
-        <h1>Justin Klitgaard</h1>
+        <h1 className="text-2xl font-bold text-accent">Justin Klitgaard</h1>
       </Link>
-      <div className="nav-links">
-        <Link to={"/"}>Work</Link>
-        <Link to={"/about"}>About Me</Link>
-        <Link to={"/resume"}>Resume</Link>
+      <div className="flex gap-1 md:gap-4">
+        <Link to={"/"} className="text-accent underline hover:text-secondary">
+          Work
+        </Link>
+        <Link
+          to={"/about"}
+          className="text-accent underline hover:text-secondary"
+        >
+          About Me
+        </Link>
+        <Link
+          to={"/resume"}
+          className="text-accent underline hover:text-secondary"
+        >
+          Resume
+        </Link>
       </div>
     </nav>
   );
