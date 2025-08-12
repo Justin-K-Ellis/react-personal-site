f7dff7760f7713bd2c9fc0e3cf5070b55f223bc4
Justin-K-Ellis <jlke45@gmail.com>
2025-08-12 22:44:08 +0900

    fix: place navbar comp in header element

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -6,7 +6,9 @@ import Navbar from "../components/Navbar";
 export default function Layout() {
   return (
     <div className="flex flex-col min-h-screen">
-      <Navbar />
+      <header>
+        <Navbar />
+      </header>
       <main className="flex w-9/10 md:w-6/10 mx-auto grow-1">
         <Outlet />
       </main>
