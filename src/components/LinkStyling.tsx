8b1480c483b2a07aa9517c5791341d00a5569302
Justin-K-Ellis <jlke45@gmail.com>
2025-08-13 22:23:25 +0900

    fix: links are always underlined and fade on hover

--- src/components/LinkStyling.tsx
+++ src/components/LinkStyling.tsx
@@ -6,6 +6,8 @@ interface Props {
 
 export default function LinkStyling({ children }: Props) {
   return (
-    <div className=" md:py-1 md:px-2 rounded hover:underline">{children}</div>
+    <div className=" md:py-1 md:px-2 rounded underline hover:opacity-65">
+      {children}
+    </div>
   );
 }
