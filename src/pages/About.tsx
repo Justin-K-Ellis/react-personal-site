fed526fbaf5a8d2222d36e0a48aef5a0d043cd41
Justin-K-Ellis <jlke45@gmail.com>
2025-09-04 10:39:51 +0900

    feat: update font to serif

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -18,27 +18,31 @@ export default function About() {
         <Card>
           <CardHeader>
             <CardTitle>
-              <h2 className="text-3xl">About Me</h2>
+              <h2 className="text-3xl font-serif">About Me</h2>
             </CardTitle>
           </CardHeader>
           <CardContent className="flex flex-col gap-2">
-            <p className="text-justify">{about.para1}</p>
-            <p className="text-justify">{about.para2}</p>
-            <p className="text-justify">{about.para3}</p>
-            <p className="text-justify">{about.para4}</p>
-            <h3 className="font-bold">In Sum:</h3>
-            <p className="text-justify">{about.inSum}</p>
+            <p className="text-justify font-serif">{about.para1}</p>
+            <p className="text-justify font-serif">{about.para2}</p>
+            <p className="text-justify font-serif">{about.para3}</p>
+            <p className="text-justify font-serif">{about.para4}</p>
+            <h3 className="font-bold font-serif">In Sum:</h3>
+            <p className="text-justify font-serif">{about.inSum}</p>
           </CardContent>
           <CardFooter>
             <ul>
               <li>
                 <LinkStyling>
-                  <a href={text.contact.linkedin}>LinkedIn</a>
+                  <a href={text.contact.linkedin} className="font-serif">
+                    LinkedIn
+                  </a>
                 </LinkStyling>
               </li>
               <LinkStyling>
                 <li>
-                  <a href={text.contact.github}>GitHub</a>
+                  <a href={text.contact.github} className="font-serif">
+                    GitHub
+                  </a>
                 </li>
               </LinkStyling>
             </ul>
@@ -51,21 +55,23 @@ export default function About() {
         <Card>
           <CardHeader>
             <CardTitle>
-              <h3>References</h3>
+              <h3 className=" font-serif">References</h3>
             </CardTitle>
           </CardHeader>
           <CardContent>
             <ul className="list-disc ml-4 flex flex-col gap-2">
               <li>
-                <p className="font-bold">The Odin Project</p>
-                <p>{about.other.odin.description}</p>
+                <p className="font-bold font-serif">The Odin Project</p>
+                <p className=" font-serif">{about.other.odin.description}</p>
                 <LinkStyling>
-                  <a href={about.other.odin.link}>Link</a>
+                  <a href={about.other.odin.link} className=" font-serif">
+                    Link
+                  </a>
                 </LinkStyling>
               </li>
               <li>
-                <p className="font-bold">Code Chrysalis</p>
-                <p>{about.other.CC.description}</p>
+                <p className="font-bold font-serif">Code Chrysalis</p>
+                <p className=" font-serif">{about.other.CC.description}</p>
                 <LinkStyling>
                   <a href={about.other.CC.link}>Link</a>
                 </LinkStyling>
