20c9b6fd3ef6603b29c6d98419269a61c26401c6
Justin-K-Ellis <jlke45@gmail.com>
2025-09-04 10:34:54 +0900

    feat: update font to mono

--- src/components/Blurb.tsx
+++ src/components/Blurb.tsx
@@ -5,7 +5,7 @@ export default function Blurb() {
   return (
     <Card className="mb-6 md:mb-12 md:mt-6 md:p-10 md:w-2/3">
       <CardContent>
-        <p className="text-justify text-xl">{text.blurb}</p>
+        <p className="text-justify text-xl font-mono">{text.blurb}</p>
       </CardContent>
     </Card>
   );
